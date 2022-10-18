0f6109a375e91d153b1b80189e3514dd6bc07cc7
Hisham Sanimeh <h.snaimeh93@gmail.com>
2022-10-18 19:15:26 -0400

    Finish main activity design

---
+++ app/src/main/java/com/centennial/mapd711/hisham/test/OutputActivity.kt
@@ -0,0 +1,11 @@
+package com.centennial.mapd711.hisham.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_output)
+    }
+}
